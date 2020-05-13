934bc05cf421c07ffd71fdf235929a8f8c208f4d
Alejandro Cruz <alejandro@gmail.com>
2020-05-13 17:50:22 -0300

    Ejercicio N°2- Variables

---
+++ Ejercicio N°2- Variables/variables.java
@@ -0,0 +1,16 @@
+
+public class variables {
+
+	public static void main(String[] args) {
+
+		int dato1 = 5;
+		int dato2 = 10;
+		int resultado = 0;
+
+		resultado = dato1 + dato2;
+
+		System.out.println("El resultado es de la suma de " + dato1 + "+" + dato2 + " es = " + resultado);
+
+	}
+
+}
